e38330d27a7d33e3050090f27731b75c4f47dbbb
Mario Andrés Rosales Peña <mariorosales@ciencias.unam.mx>
2024-02-24 07:29:10 -0600

    Modificando Script 1

--- Script1.py
+++ Script1.py
@@ -14,9 +14,10 @@ def insertar_registros():
     try:
         conn = pymysql.connect(**config)
         cursor = conn.cursor()
+        5
         cursor.execute("INSERT INTO usuarios (nombre, password, email, superUser) VALUES ('Usuario'+str(num_usuario), 'password'+str(num_usuario)+str(num_usuario+1)+str(num_usuario+2), 'usuario'+str(num_usuario)+'@gmail.com', 0)")
         cursor.execute("INSERT INTO peliculas (nombre, genero, duracion, inventario) VALUES ('Pelicula'+str(num_usuario), 'Acción', 120, 10)")
-        # Comprueba que exista un usuario y una pelicula para poder insertar un registro en la tabla rentar
+
         cursor.execute("SELECT * FROM usuarios")
         usuario = cursor.fetchone()
         cursor.execute("SELECT * FROM peliculas")
@@ -27,7 +28,10 @@ def insertar_registros():
             print("No se pudo insertar el registro en la tabla rentar porque no existen más registros en las tablas usuarios y peliculas")
         conn.commit()
     except Exception as e:
-        print("No se pudo insertar el registro: ", e)
+        print("No se pudo insertar el registro.", e)
     finally:
         cursor.close()
         conn.close()
+
+
+insertar_registros()
